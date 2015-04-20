adfc68bb9e4c62710403925adddff838993c59e2
Leelow <leo.lozach@insa-rennes.fr>
2015-04-20 19:20:26 +0200

    Update index.php #3.1

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 				  <h4 class="modal-title">Campagnes à venir</h4>
 				</div>
 				<div class="modal-body">
-				  <p>One fine body…</p>
+				  <p><?php displayScheduledCampaign() ?></p>
 				</div>
 				<div class="modal-footer">
 				  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>

6efa60e1fb8a70a3bbbdecb98e9aa372b19dfd65
Leelow <leo.lozach@insa-rennes.fr>
2015-04-20 21:38:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -59,26 +59,13 @@
 		    </div>
 		</div>
     </div>
-	
-	<div class="row">
-		<h4>Campagnes à venir</h4>
-	</div>
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="bs-component">
-			  <div class="modal">
-				<div class="modal-dialog">
-				  <div class="modal-content">
-					<div class="modal-body">				
-					  <p>Depuis le <b><?php echo $first_date ?></b>, <b><?php echo $number_tweets ?></b> de tweets ont été récoltés sur <b><?php echo $number_campaign  . ' ' . $print_campagnes ?></b>. Cela représente environ <b><?php echo $size_tweets ?></b>.</p>
-					</div>
-				  </div>
-				</div>
-			  </div>
-		    </div>
-		</div>
-	</div>	
 
+		<!-- Progress footer -->
+        <div id="progress-footer" class="navbar navbar-default navbar-fixed-bottom" style="display:none">
+			<div class="container">
+            </div>
+        </div>
+	
 <?php
 
 	require_once DIR_INCLUDES . 'footer.inc.php';;

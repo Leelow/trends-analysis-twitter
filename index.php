90b0e51d5be9163b1a0a1de109d5b82846f339f7
Leelow <leo.lozach@insa-rennes.fr>
2015-04-20 20:00:05 +0200

    Update index.php #5

--- index.php
+++ index.php
@@ -24,27 +24,19 @@
 		  </div>
 	  </div>
 	  <div class="col-lg-6">
-		<h2>Popovers</h2>
 		<div class="bs-component">
-		  <button title="" data-original-title="" type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">Left</button>
-
-		  <button title="" data-original-title="" type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">Top</button>
-
-		  <button title="" data-original-title="" type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus
-		  sagittis lacus vel augue laoreet rutrum faucibus.">Bottom</button>
-
-		  <button title="" data-original-title="" type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">Right</button>
-		</div>
-		<h2>Tooltips</h2>
-		<div class="bs-component">
-		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="" data-original-title="Tooltip on left">Left</button>
-
-		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="top" title="" data-original-title="Tooltip on top">Top</button>
-
-		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom">Bottom</button>
-
-		  <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="" data-original-title="Tooltip on right">Right</button>
-		</div>
+		  <div class="modal">
+			<div class="modal-dialog">
+			  <div class="modal-content">
+				<div class="modal-header">
+				  <h4 class="modal-title">Campagnes à venir</h4>
+				</div>
+				<div class="modal-body">
+				  <p><?php displayScheduledCampaign() ?></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
 	  </div>
     </div>
 
